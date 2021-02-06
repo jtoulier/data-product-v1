2a5294e6b71ff33e03c301c21dfdc64a78e901bf
Joseph Toulier Pighi <jtoulier@gmail.com>
2021-02-06 01:09:18 -0500

    configuracion de config-server

---
+++ src/main/java/com/reto2/dataproductv1/DataProductV1Application.java
@@ -0,0 +1,13 @@
+package com.reto2.dataproductv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProductV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProductV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto2/dataproductv1/DataProductV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto2.dataproductv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProductV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
